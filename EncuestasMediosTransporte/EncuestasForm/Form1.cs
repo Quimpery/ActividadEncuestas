d9818c0cd95716b1c021cb76d118deee32aa593f
Quimpery <lautarogomgoda@gmail.com>
2023-06-15 22:40:38 -0300

    agregando proyecto

---
+++ EncuestasMediosTransporte/EncuestasForm/Form1.cs
@@ -0,0 +1,65 @@
+﻿using EncuestasLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncuestasForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistroDeEncuesta_Click(object sender, EventArgs e)
+        {
+
+            FormRegistroDeEncuesta fRegistro = new FormRegistroDeEncuesta();
+
+            if (fRegistro.ShowDialog() == DialogResult.OK)
+            {
+
+
+                Encuesta nuevo = new Encuesta();
+
+                // Console.WriteLine("Modo de transporte habitual");
+
+                nuevo.UsaBicicleta = fRegistro.cbkUsaBicicleta.Checked; // Consulta("¿Usa bicleta?: S/N");
+                nuevo.UsaAuto = false; // Consulta("¿Usa Automóvil?: S/N");
+                nuevo.UsaTransportePublico = false; // Consulta("¿Usa Transporte público?: S/N");
+
+                // Console.WriteLine("¿Cuál es la distancia aproximada a su destino de trabajo/estudio en km? (ej:1,5)\n");
+                nuevo.DistanciaASuDestino = 3.0; //Convert.ToDouble(Console.ReadLine());
+
+                bool puedeSerContactado = true; // Consulta("¿Puede ser contactado?: S/N");
+                if (puedeSerContactado == true)
+                {
+                    //Console.Write("Email: ");
+                    nuevo.Email = "lautarogomgoda@gmail.com"; //Console.ReadLine();
+                                                              // Console.Write("\n");
+                }
+
+
+                proceso.RegistrarEncuesta(nuevo, puedeSerContactado);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FormResultados fInforme = new FormResultados();
+
+            fInforme.listBox1.Items.Add($"{"Bicicleta:",-20}  {}")
+
+            fInforme.ShowDialog();
+        }
+    }
+}
---
+++ EncuestasMediosTransporte/EncuestasForm/FormRegistroDeEncuesta.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace EncuestasForm
+{
+    partial class FormRegistroDeEncuesta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbkUsaBicicleta = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbkUsaBicicleta);
+            this.groupBox1.Location = new System.Drawing.Point(73, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 145);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // cbkUsaBicicleta
+            // 
+            this.cbkUsaBicicleta.AutoSize = true;
+            this.cbkUsaBicicleta.Location = new System.Drawing.Point(6, 36);
+            this.cbkUsaBicicleta.Name = "cbkUsaBicicleta";
+            this.cbkUsaBicicleta.Size = new System.Drawing.Size(121, 20);
+            this.cbkUsaBicicleta.TabIndex = 0;
+            this.cbkUsaBicicleta.Text = "¿Usa bicicleta?";
+            this.cbkUsaBicicleta.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(79, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(230, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // FormRegistroDeEncuesta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 384);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormRegistroDeEncuesta";
+            this.Text = "FormRegistroDeEncuesta";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        public System.Windows.Forms.CheckBox cbkUsaBicicleta;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
